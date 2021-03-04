92d8c889a68d46c53e8c5fad9cfdb81d051d7c82
Gregor Seegers <gregor.seegers@gmail.com>
2021-03-04 20:31:17 +0100

    complete intermediate exercise 3

---
+++ 012_JavaScript-Day1-Exercises/Intermediate-Exercise3.js
@@ -0,0 +1,3 @@
+var fruits = "mango/cherries/kiwi/grapes/pear/peach/orange/lemon";
+var fruits2 = fruits.split("/");
+console.log(`${fruits2[0]}\n${fruits2[1]}\n${fruits2[2]}\n${fruits2[3]}\n${fruits2[4]}\n${fruits2[5]}\n${fruits2[6]}\n${fruits2[7]}`);
