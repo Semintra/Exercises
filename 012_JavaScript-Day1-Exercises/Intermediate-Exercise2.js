b3e557d0dfbd1b03e7a8880b14cb7c68088260da
Gregor Seegers <gregor.seegers@gmail.com>
2021-03-04 19:43:51 +0100

    complete intermediate exercise 2

---
+++ 012_JavaScript-Day1-Exercises/Intermediate-Exercise2.js
@@ -0,0 +1,15 @@
+var fruits = ["apple", "banana", "kiwi"];
+
+fruits.push("orange");
+console.log(fruits);
+
+fruits.pop("orange");
+console.log(fruits);
+
+var animals = ["monkey", "horse", "dog"];
+
+animals.sort();
+console.log(animals);
+
+animals.unshift("cat");
+console.log(animals);
