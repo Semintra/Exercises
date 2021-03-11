cd102cea37eb4ebec95876e672494a1613db401c
Gregor Seegers <gregor.seegers@gmail.com>
2021-03-11 19:15:49 +0100

    complete intermediate exercise

---
+++ 017_JavaScript-Day6-Exercises/Intermediate-Exercise/Intermediate-Exercise.js
@@ -0,0 +1,22 @@
+// ### Parse JSON-file ###
+
+var employeeTable = document.getElementById("employeeTable");
+
+// ### Build table ###
+
+for (i = 0; i < employees.length; i++) {
+  employeeTable.innerHTML +=
+    "<tr><td>" +
+    employees[i].id +
+    "</td><td>" +
+    employees[i].first_name +
+    "</td><td>" +
+    employees[i].last_name +
+    "</td><td>" +
+    employees[i].email +
+    "</td><td>" +
+    employees[i].job_title +
+    "</td><td>" +
+    employees[i].salary +
+    "</td></tr>";
+}
