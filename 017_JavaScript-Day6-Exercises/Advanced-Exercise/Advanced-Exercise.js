6df19c137a925fce4cc62b3d2ef2e7adc72aa1fb
Gregor Seegers <gregor.seegers@gmail.com>
2021-03-11 21:51:10 +0100

    advanced exercise part 1

--- 017_JavaScript-Day6-Exercises/Advanced-Exercise/Advanced-Exercise.js
+++ 017_JavaScript-Day6-Exercises/Advanced-Exercise/Advanced-Exercise.js
@@ -7,7 +7,21 @@ let books = JSON.parse(booklist);
 var shelf = document.getElementById("bookshelf");
 
 for (i = 0; i < books.length; i++) {
-  shelf.innerHTML += "<p>" + books[i].title + "<br>" + books[i].author + "</p>";
+  var boolCheck = books[i].read;
+  shelf.innerHTML +=
+    "<img src=" +
+    books[i].image +
+    "><p>" +
+    books[i].title +
+    "<br>" +
+    books[i].author +
+    "</p>";
+  console.log(boolCheck);
+  if (boolCheck) {
+    shelf.style.color = "green";
+  } else {
+    shelf.style.color = "red";
+  }
 }
 
-console.table(books);
+console.log(shelf);
